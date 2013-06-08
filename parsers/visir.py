be9dce817a934c5a7744ea688c8d1a28f828fdeb
Arnór Bogason <arnor@arnor.is>
2013-06-08 10:28:24 +0000

    visir parser ready
    visir parser should be tested thoroughly, but it works. result are very
    raw, but the vísir article html is very hard to work with and not
    accessible at all.

--- parsers/visir.py
+++ parsers/visir.py
@@ -2,7 +2,6 @@ from baseparser import BaseParser
 from BeautifulSoup import BeautifulSoup, Tag
 
 
-
 class VisirParser(BaseParser):
     domains = ['www.visir.is']
 
@@ -12,13 +11,34 @@ class VisirParser(BaseParser):
     def _parse(self, html):
         soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES,
                              fromEncoding='utf-8')
+        
+        elt = soup.find('h1')
+        if elt is None:
+            self.real_article = False
+            return
+        self.title = elt.getText()
 
-        tmp = soup.find('div', 'paragraph')
-        print tmp
-        # tmp = tmp.replace('<br /><br />', '</p><p>')
+        tmp = str(soup.find('div', 'paragraph'))
 
-        self.real_article = True
-        self.date = ''
-        self.body = ''
-        self.byline = ''
-        self.title = ''
+        if tmp is None:
+            self.real_article = False
+            return
+        tmp2 = tmp.replace('<br /><br />', '</p><p>')
+
+        moresoup = BeautifulSoup(tmp2)
+        allp = moresoup.findAll('p')
+
+
+        bymeta = str(soup.find('div', 'meta'))
+        by = bymeta.replace(' skrifar:', '')
+        newby = BeautifulSoup(by)
+
+        self.byline = newby.getText()
+        
+        
+        
+        
+        # print moresoup
+
+        self.date = soup.find('div', 'article').find('span', 'date').getText()
+        self.body = '\n'+'\n\n'.join([x.getText() for x in allp])
